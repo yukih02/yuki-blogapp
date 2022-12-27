e84e7c72760f1839057078753f4991bfd16e3ca3
yukih02 <2yiku31@gmail.com>
2022-12-28 03:33:19 +0900

    フォームを部分テンプレートに変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   root to: 'articles#index'
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :articles, only: [:show, :new, :create, :edit, :update, :destroy]
-
+  resources :articles
 end
